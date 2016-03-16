8dfca4d9832b11f8ac6ec5b575acad76ee1d6b78
pirhoo <hello@pirhoo.com>
2016-03-16 16:06:02 +0100

    Added mailtrap for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,10 +61,6 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
@@ -74,4 +70,31 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Ignore bad email addresses and do not raise email delivery errors.
+  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'jquest.herokuapp.com', port: 80 }
+
+  if ENV.has_key?("MAILTRAP_API_TOKEN")
+    # Get Mailtrap SMTP details using its API
+    url = "https://mailtrap.io/api/v1/inboxes.json?api_token=#{ENV['MAILTRAP_API_TOKEN']}"
+    begin
+      response = RestClient.get url, {:accept => :json}
+      # Get first inbox
+      inbox = JSON.parse(response)[0]
+
+      config.action_mailer.delivery_method = :smtp
+      config.action_mailer.smtp_settings = {
+        :user_name => inbox['username'],
+        :password => inbox['password'],
+        :address => inbox['domain'],
+        :domain => inbox['domain'],
+        :port => inbox['smtp_ports'][0],
+        :authentication => :plain
+      }
+    rescue RestClient::Unauthorized
+      p 'Unable to configure Mailtrap'
+    end
+  end
 end

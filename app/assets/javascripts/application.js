3f68429959633ef3f7b0ebc900dfc2646a11d4e6
pirhoo <hello@pirhoo.com>
2016-03-16 16:12:58 +0100

    Removed useless dep

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,4 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require turbolinks
 //= require_tree ../client

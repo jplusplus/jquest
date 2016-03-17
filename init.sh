39b662fbb0fbc934dc5c507903d76ab60581b6b7
pirhoo <hello@pirhoo.com>
2016-03-17 17:30:59 +0100

    Improve init script

--- init.sh
+++ init.sh
@@ -4,4 +4,13 @@ for SCRIPT in /app/.profile.d/*;
   do source $SCRIPT;
 done
 
+# Create, migrate DB
+if rake db:version | grep ": 0$"; then
+  echo 'Creating database'
+  rake db:setup
+else
+  echo 'Migrating database'
+  rake db:migrate
+fi
+
 exec "$@"

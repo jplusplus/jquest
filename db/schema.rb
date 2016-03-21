9df190c62945a1c9ae779c2d0dba463c0c5ee75e
pirhoo <hello@pirhoo.com>
2016-03-21 15:37:22 +0100

    Added ngAnnotate

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160319125541) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "jquest_pg_territories", force: :cascade do |t|
     t.string   "name"
     t.string   "country"
@@ -27,8 +30,8 @@ ActiveRecord::Schema.define(version: 20160319125541) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "school_teachers", ["school_id"], name: "index_school_teachers_on_school_id"
-  add_index "school_teachers", ["user_id"], name: "index_school_teachers_on_user_id"
+  add_index "school_teachers", ["school_id"], name: "index_school_teachers_on_school_id", using: :btree
+  add_index "school_teachers", ["user_id"], name: "index_school_teachers_on_user_id", using: :btree
 
   create_table "schools", force: :cascade do |t|
     t.string   "name"
@@ -70,11 +73,11 @@ ActiveRecord::Schema.define(version: 20160319125541) do
     t.string   "phone_number",              default: "",    null: false
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["provider"], name: "index_users_on_provider"
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["uid"], name: "index_users_on_uid"
-  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["provider"], name: "index_users_on_provider", using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["uid"], name: "index_users_on_uid", using: :btree
+  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
 
 end

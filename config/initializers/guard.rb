7a2c0d9bb6c68e2a04c5965816ea6cf0965096cb
pirhoo <hello@pirhoo.com>
2016-03-25 17:22:29 +0100

    Do not initialize guard out of development

--- config/initializers/guard.rb
+++ config/initializers/guard.rb
@@ -1,14 +1,16 @@
-require 'guard'
-# needed because of https://github.com/guard/guard/issues/793
-require 'guard/commander'
+if Rails.env.development?
+  require 'guard'
+  # needed because of https://github.com/guard/guard/issues/793
+  require 'guard/commander'
 
-# Start Guard only with rails.
-if File.basename($0, '.*') == 'rails'
-  fork do
-    if __FILE__ == '(irb)'
+  # Start Guard only with rails.
+  if File.basename($0, '.*') == 'rails'
+    fork do
+      if __FILE__ == '(irb)'
+      end
+
+      # Guard.guards 'sass'
+      Guard.start :no_interactions => true
     end
-
-    # Guard.guards 'sass'
-    Guard.start :no_interactions => true
   end
 end

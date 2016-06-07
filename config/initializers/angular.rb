87519f68fe065336e89e29f3479dab06a5ae85eb
Pirhoo <hello@pirhoo.com>
2016-06-07 17:09:44 +0200

    Annotate season

--- config/initializers/angular.rb
+++ config/initializers/angular.rb
@@ -2,7 +2,13 @@
 Rails.application.config.angular_templates.module_name = 'jquest.template'
 
 Rails.application.configure do
-  config.ng_annotate.paths = [
-    Rails.root.join('app/assets').to_s,
-  ]
+  # Add the root dir
+  config.ng_annotate.paths = [ Rails.root.join('app/assets').to_s ]
+  # Analyse every engines
+  Rails::Engine.subclasses.each do |engine|
+    # Does the engine have specs? Does the specs describe a jQuest season?
+    if engine.season?
+      config.ng_annotate.paths << engine.config.root.to_s
+    end
+  end
 end

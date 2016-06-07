58262dc1e48b85452c386abb11419782b9e4d25d
Pirhoo <hello@pirhoo.com>
2016-06-07 17:16:59 +0200

    Set priority 1 to engine initializer

--- config/initializers/angular.rb
+++ config/initializers/angular.rb
@@ -2,13 +2,10 @@
 Rails.application.config.angular_templates.module_name = 'jquest.template'
 
 Rails.application.configure do
-  # Add the root dir
   config.ng_annotate.paths = [ Rails.root.join('app/assets').to_s ]
-  # Analyse every engines
-  Rails::Engine.subclasses.each do |engine|
-    # Does the engine have specs? Does the specs describe a jQuest season?
-    if engine.season?
-      config.ng_annotate.paths << engine.config.root.to_s
-    end
+  # Analyse every season's engines
+  Season::engines.each do |engine|
+    config.ng_annotate.paths << engine.config.root.to_s
+    puts engine.config.root.join('app/assets').to_s
   end
 end

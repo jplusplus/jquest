199164ee14560f9da8c06b26306628615c4da50c
Pirhoo <hello@pirhoo.com>
2016-06-07 17:17:57 +0200

    Removed debug output

--- config/initializers/angular.rb
+++ config/initializers/angular.rb
@@ -5,7 +5,6 @@ Rails.application.configure do
   config.ng_annotate.paths = [ Rails.root.join('app/assets').to_s ]
   # Analyse every season's engines
   Season::engines.each do |engine|
-    config.ng_annotate.paths << engine.config.root.to_s
-    puts engine.config.root.join('app/assets').to_s
+    config.ng_annotate.paths << engine.config.root.join('app/assets').to_s
   end
 end

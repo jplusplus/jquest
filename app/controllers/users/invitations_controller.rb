1457effe1cf962ae5674ce2f2bff2ed4b67abc32
Pirhoo <hello@pirhoo.com>
2016-06-10 15:51:24 +0200

    Added user invitation controller and views

---
+++ app/controllers/users/invitations_controller.rb
@@ -0,0 +1,31 @@
+class Users::InvitationsController < Devise::InvitationsController
+
+  # see https://github.com/scambra/devise_invitable/blob/master/app/controllers/devise/invitations_controller.rb#L52
+  def update
+    raw_invitation_token = update_resource_params[:invitation_token]
+    self.resource = accept_resource
+    invitation_accepted = resource.errors.empty?
+
+    yield resource if block_given?
+
+    if invitation_accepted
+      if Devise.allow_insecure_sign_in_after_accept
+        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+        set_flash_message :notice, flash_message if is_flashing_format?
+        sign_in(resource_name, resource)
+        # Redirect to the season this user is related to
+        if resource.group and resource.group.season
+          redirect_to resource.group.season.path
+        else
+          respond_with resource, :location => after_accept_path_for(resource)
+        end
+      else
+        set_flash_message :notice, :updated_not_active if is_flashing_format?
+        respond_with resource, :location => new_session_path(resource_name)
+      end
+    else
+      resource.invitation_token = raw_invitation_token
+      respond_with_navigational(resource){ render :edit }
+    end
+  end
+end

291d7acf04730eb00b46d3cff1f45d027086c25e
Pirhoo <hello@pirhoo.com>
2016-06-15 17:01:02 +0200

    Updated schema using jquest_pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615142942) do
+ActiveRecord::Schema.define(version: 20160615144927) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20160615142942) do
     t.integer  "number_of_members"
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.string   "languages"
   end
 
   create_table "jquest_pg_mandatures", force: :cascade do |t|

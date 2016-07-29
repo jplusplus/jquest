3d4058e6358d9cd85f563cbaab04e4d2621a7474
Pirhoo <hello@pirhoo.com>
2016-07-29 19:04:41 +0200

    Added logo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160728083455) do
+ActiveRecord::Schema.define(version: 20160729160757) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id"

84097187d7ba6fd6bc9582ae742f7b6d1c16d3b9
Pirhoo <hello@pirhoo.com>
2016-08-01 15:17:56 +0200

    Better invitation email #9

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-
+  def devise_invitation
+    Devise::Mailer.invitation_instructions(User.first, 'dummy token')
+  end
 end

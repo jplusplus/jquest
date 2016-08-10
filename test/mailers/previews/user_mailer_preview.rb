639bb9f75a2a8f4223e798bcc9d85ccc3e20f1f9
Pirhoo <hello@pirhoo.com>
2016-08-10 18:22:42 +0200

    Better confirmation email

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,6 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-  def devise_invitation
+  def devise_invitation_instructions
     Devise::Mailer.invitation_instructions(User.first, 'dummy token')
+  end
+
+  def devise_confirmation_instructions
+    Devise::Mailer.confirmation_instructions(User.first, 'dummy token')
   end
 end

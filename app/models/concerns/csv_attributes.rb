1e35569318c8849f6728f10c0ad9fc67bbb3205c
Pirhoo <hello@pirhoo.com>
2016-09-14 12:25:14 +0200

    Faster csv generator

--- app/models/concerns/csv_attributes.rb
+++ app/models/concerns/csv_attributes.rb
@@ -56,13 +56,33 @@ module CsvAttributes
       csv_attributes_literal + csv_attributes_associated
     end
 
+    def self.sliced_each(batch_size: 1000)
+      # Shortcut to limit value (if any)
+      limit_value = all.limit_value || count
+      offset_value = all.offset_value || 0
+      # Only a slice, bigger than the batch size
+      if limit_value > batch_size
+        # Loop a fixed number of times
+        (limit_value/batch_size.to_f).ceil.times do |index|
+          # Calculate offset and limit dynamicly using batch size,
+          # current index and original offset value
+          offset = offset_value + index * batch_size
+          limit = [limit_value - index * batch_size, batch_size].min
+          # Iterate over a given batch
+          offset(offset).limit(limit).each { |o| yield o }
+        end
+      else
+        all.each { |o| yield o }
+      end
+    end
+
     def self.to_csv
       CSV.generate(headers: true) do |csv|
         attributes = csv_attributes_all
         # Add headers
         csv << attributes
         # For all given object
-        all.each do |object|
+        all.sliced_each do |object|
           values = []
           # Get all literal values
           values += csv_attributes_literal.map do |attr|

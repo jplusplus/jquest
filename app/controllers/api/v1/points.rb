17fe3d1aba2f6948d76d185f7c736c473055e3ca
Pirhoo <hello@pirhoo.com>
2016-09-23 12:05:11 +0200

    Added cache with garner

--- app/controllers/api/v1/points.rb
+++ app/controllers/api/v1/points.rb
@@ -10,16 +10,20 @@
         end
         paginate
         get do
-          paginate policy_scope(Point).
-            # Only positive points
-            where('value > ?', 0).
-            # We allow filtering
-            search(declared params).
-            result.
-            # Join resources
-            eager_load(:user).
-            # Order by position
-            order(value: :desc)
+          garner do
+            paginate policy_scope(Point).
+              # Only positive points
+              where('value > ?', 0).
+              # We allow filtering
+              search(declared params).
+              result.
+              # Join resources
+              eager_load(:user).
+              # Order by position
+              order(value: :desc).
+              # Allow caching
+              to_a
+          end
         end
       end
     end

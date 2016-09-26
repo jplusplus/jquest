91d8621a07018756454ad9bc2614a36a13c4b2f2
Pirhoo <hello@pirhoo.com>
2016-09-26 18:12:07 +0200

    Debug "in_season" concern

--- app/models/concerns/in_season.rb
+++ app/models/concerns/in_season.rb
@@ -11,7 +11,7 @@ module InSeason
       # If a season is not given explicitely..
       if self.season.nil? and not self.user.nil? and not self.user.member_of.nil?
         # ...we deduce it from the user's group
-        self.season = user.member_of
+        self.season_id = user.member_of.id
       end
     end
 end

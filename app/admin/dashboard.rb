6c46fa54548db09c15476c559089f9306d92c538
Pirhoo <hello@pirhoo.com>
2016-11-21 15:21:18 +0100

    Restore empty dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,21 +3,24 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
+    # Here is an example of a simple dashboard with columns and panels.
+    #
+    # columns do
+    #   column do
+    #     panel "Recent Posts" do
+    #       ul do
+    #         Post.recent(5).map do |post|
+    #           li link_to(post.title, admin_post_path(post))
+    #         end
+    #       end
+    #     end
+    #   end
 
-    columns do
-      column do
-        panel "Updates" do
-          section "Recently updated content" do
-            table_for PaperTrail::Version.order('id desc').limit(10) do # Use PaperTrail::Version if this throws an error
-              column ("Item") { |v| v.item }
-              # column ("Item") { |v| link_to v.item, [:admin, v.item] } # Uncomment to display as link
-              column ("Type") { |v| v.item_type.underscore.humanize }
-              column ("Modified at") { |v| v.created_at.to_s :long }
-              column ("Author") { |v| link_to User.find(v.whodunnit).email, [:admin, User.find(v.whodunnit)] }
-            end
-          end
-        end
-      end
-    end
+    #   column do
+    #     panel "Info" do
+    #       para "Welcome to ActiveAdmin."
+    #     end
+    #   end
+    # end
   end # content
 end

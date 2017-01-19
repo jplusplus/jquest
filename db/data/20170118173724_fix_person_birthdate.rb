6c0fb43a40540f89b4d2b56fae690c0fc9421758
pirhoo <hello@pirhoo.com>
2017-01-19 16:17:28 +0100

    Filter birthdate fi to non-null values

--- db/data/20170118173724_fix_person_birthdate.rb
+++ db/data/20170118173724_fix_person_birthdate.rb
@@ -3,7 +3,7 @@ class FixPersonBirthdate < SeedMigration::Migration
     # Collect id of every person where a sourced birthday have been provided
     ids = Source.where(resource_type: 'JquestPg::Person', field: 'birthdate').pluck(:id)
     # For every person, we have to look into the version
-    JquestPg::Person.where(id: ids).find_each do |person|
+    JquestPg::Person.where(id: ids).where.not(birthdate: nil).find_each do |person|
       # Add one date to every birthdate
       person.update_attribute :birthdate, person.birthdate + 1.day
     end

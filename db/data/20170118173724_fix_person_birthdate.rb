a5d727135097536c715620cc50fcb62f2d8ec51a
pirhoo <hello@pirhoo.com>
2017-01-20 12:49:04 +0100

    Fixed fixer

--- db/data/20170118173724_fix_person_birthdate.rb
+++ db/data/20170118173724_fix_person_birthdate.rb
@@ -1,7 +1,7 @@
 class FixPersonBirthdate < SeedMigration::Migration
   def up
     # Collect id of every person where a sourced birthday have been provided
-    ids = Source.where(resource_type: 'JquestPg::Person', field: 'birthdate').pluck(:id)
+    ids = Source.where(resource_type: 'JquestPg::Person', field: 'birthdate').pluck(:resource_id)
     # Get every person witin those ids and with a birthday
     JquestPg::Person.where(id: ids).where.not(birthdate: nil).find_each do |person|
       # Add one date to every birthdate

019bf0aa9aa4b18549a31f7c3fd2938b95f5ebe1
pirhoo <hello@pirhoo.com>
2017-02-06 19:00:05 +0100

    Added tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,12 +27,17 @@ Rails.application.configure do
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
-  config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,16 @@ RSpec.describe User, type: :model do
     expect(User.count).to eq 1
   end
 
+  it "saves email in lowercase" do
+    User.create email: 'CONTACT@jplusplus.org'
+    expect(User.first.email).to eq 'contact@jplusplus.org'
+  end
+
+  it "has md5 email" do
+    User.create email: 'contact@jplusplus.org'
+    expect(User.first.email_md5).to eq '0a836ee9e71fb926c2c2cf649b7b6047'
+  end
+
   it "still has one after adding one" do
     User.create email: 'contact@jplusplus.org'
     expect(User.count).to eq 1
@@ -31,5 +41,16 @@ RSpec.describe User, type: :model do
   it "has one with no assignments" do
     User.create email: 'enroll@jquestapp.com'
     expect(User.first.assignments.count).to eq 0
+  end
+
+  it "should be inactive" do
+    User.create email: 'enroll@jquestapp.com'
+    expect(User.first.status).to be :inactive
+  end
+
+  it "should be invited" do
+    user = User.create email: 'enroll@jquestapp.com'
+    user.invite!
+    expect(User.first.status).to be :invited
   end
 end

9fafb0f64ffa40322bbf96103b0ea8d8bbacc800
pirhoo <hello@pirhoo.com>
2017-02-07 18:32:27 +0100

    Better user default for factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,8 +4,21 @@ FactoryGirl.define do
     lastname  "Doe"
     role 'student'
     home_country 'FR'
+    spoken_language 'fr'
+    group
     sequence(:email) do |n|
       "#{firstname}.#{lastname}.#{n}@jquestapp.com".downcase
     end
+  end
+
+  factory :group do
+    season
+  end
+
+  factory :season do
+    sequence(:name) do |n|
+      "Season #{n}"
+    end
+    engine_name 'JquestPg'
   end
 end
